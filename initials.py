7a9a50aa7df537f5ffd4dd49ceaadde906a8d872
Rahmaa792 <2425403392@live.gctu.edu.gh>
2026-06-01 11:39:22 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Rahma Ibrahim
+# STUDENT ID: 2425403392
+# Email: 2425403392@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Rahma Ibrahim, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials
 
+    print()
+    print("...RIR")
+    print()
+
+    print("...RRRRRRRRRR..IIIIIIIIIIII..RRRRRRRRRRRR....")
+    print("...RR.......RR.....II.......RR.......RR....")
+    print("...RR.......RR.....II.......RR.......RR....")
+    print("...RR.......RR.....II.......RR.......RR....")
+    print("...RRRRRRRRRR......II.......RRRRRRRRRRR....")
+    print("...RRRRRR..........II.......RRRRRR.........")
+    print("...RR..RR..........II.......RR..RR.........")
+    print("...RR...RR.........II.......RR...RR........")
+    print("...RR....RR....IIIIIIIIIIII.RR....RR...**")
+    print("...RR.....RR...IIIIIIIIIIII.RR.....RR..**")
+
+    print()
 
 
 main()
